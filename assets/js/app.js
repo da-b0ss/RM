550fd28a66164728804e50883c228193300b9a06
Robert Medina <66793722+da-b0ss@users.noreply.github.com>
2024-10-25 06:16:22 -0400

    almost working

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
+const scrollUp = document.querySelector("#scroll-up");
 
 burger.addEventListener("click", () => {
     ul.classList.toggle("show");
